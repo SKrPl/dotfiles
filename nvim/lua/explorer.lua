8cdbda274823f02c487f3e982a4eca6098ebe519
Siddhant Kumar Patel <siddhantkumar.patel97@gmail.com>
2023-02-25 12:25:08 +0530

    Add shortcuts for nvim-tree commands

--- nvim/lua/explorer.lua
+++ nvim/lua/explorer.lua
@@ -2,3 +2,8 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup()
+
+-- toggle keymap
+vim.keymap.set('n', '<leader>nt', ':NvimTreeToggle<CR>')
+-- find the current buffer in explorer
+vim.keymap.set('n', '<leader>nf', ':NvimTreeFindFile<CR>')
